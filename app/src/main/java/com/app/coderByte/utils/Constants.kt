c17c2a9a86f6146e3d5e981e88ef879b1bd46f4d
Shafaq <shafaq@nextgeni.com>
2021-03-07 14:19:08 +0500

    Architecture|Cashing|UI

--- app/src/main/java/com/app/coderByte/utils/Constants.kt
+++ app/src/main/java/com/app/coderByte/utils/Constants.kt
@@ -5,6 +5,4 @@ internal object Constants {
     const val API_RESPONSE_CODE_200 = 200
     const val API_RESPONSE_CODE_401 = 401
     const val API_RESPONSE_CODE_500 = 500
-    const val DISK_CACHE_SIZE = 1024 * 1024 * 10 // 10MB
-    const val DISK_CACHE_SUBDIR = "thumbnails"
 }
